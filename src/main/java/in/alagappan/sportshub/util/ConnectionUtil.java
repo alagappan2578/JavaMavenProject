10e34101bed017875fe3cfc3d1da0c9eb2fb6fe1
alagappan2578 <116268706+alagappan2578@users.noreply.github.com>
2023-07-25 12:18:34 +0530

    jdbc connection updated

--- src/main/java/in/alagappan/sportshub/util/ConnectionUtil.java
+++ src/main/java/in/alagappan/sportshub/util/ConnectionUtil.java
@@ -6,21 +6,21 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class ConnectionUtil {
 	// this method use when ever we connect with database. 
 	public static Connection getConnection() {
+		
+		Dotenv env = Dotenv.load();
+		String url = env.get("DATABASE_HOST");
+		String userName = env.get("DATABASE_USERNAME");
+		String password = env.get("DATABASE_PASSWORD");
+		
 		Connection connection = null;
-		String url = null;
-		String userName = null;
-		String password = null;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-				connection = DriverManager.getConnection(
-			  "jdbc:mysql://aws.connect.psdb.cloud/demo_project?sslMode=VERIFY_IDENTITY",
-			  "mmrhu53pr1829maijp8m",
-			  "pscale_pw_q3QGD7QBYpgelODpVy30OpNHorGgx6fT2JPV2XS53p");
-//			Class.forName("com.mysql.cj.jdbc.Driver");
-//			connection = DriverManager.getConnection(url, userName, password);
+				connection = DriverManager.getConnection(url,userName,password);
 		}catch(Exception e) {
 			e.printStackTrace();
 			throw new RuntimeException(e);
